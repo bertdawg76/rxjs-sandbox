1a2f3290ab1fc623d2ddaff461de38d42ad87225
bertdawg76 <bertcoleman76@gmail.com>
2017-07-11 09:00:16 -0600

    more stuff

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -6408,51 +6408,47 @@ var _getSubscriber = __webpack_require__(352);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// Rx.Observable.range(0, 10)
-//   .filter(function (x, i, obs) {
-//     return x >= 5;
-//   })
-//   .subscribe(getSubscriber("filter"));
+// Rx.Observable.of("hello")
+//   .merge(Rx.Observable.of("world"))
+//   .subscribe(getSubscriber("merge"));
 
-// Rx.Observable.range(0, 10)
-//   .filter(function (x, i, obs) {
-//     return x % 2 === 0;
-//   })
-//   .subscribe(getSubscriber("filter"));
+// Rx.Observable.interval(2000)
+//   .merge(Rx.Observable.interval(500))
+//   .take(25)
+//   .subscribe(getSubscriber("merge"));
 
-var input = (0, _jquery2.default)('#sku');
-var output = (0, _jquery2.default)('#output');
-
-// let products = [
-//   {sku:"001", name: "red t-shirt", price: 4.99},
-//   {sku:"002", name: "blue pants", price: 9.99},
-//   {sku:"003", name: "green hat", price: 7.99},
-//   {sku:"003", name: "yellow t-shirt", price: 4.99},
-// ];
+//let source1$ = Rx.Observable.interval(2000).map(i => 'M1: ' + i);
+//let source2$ = Rx.Observable.interval(500).map(i => 'M2: ' + i);
 //
-// Rx.Observable.fromEvent(input, 'keyup')
-//   .map(e => e.target.value)
-//   .subscribe(x => {
-//     Rx.Observable.from(products)
-//       .filter(i => i.sku == x)
-//       .subscribe(x => {
-//         output.html('<h1>'+x.name+'</h1><h3>$'+x.price+'</h3>')
-//       })
-//   })
-//
-// Rx.Observable.from(products)
-//   .filter(i => i.price < 5)
-//   .subscribe(x => {
-//     console.log(x)
-//   })
+// Rx.Observable.merge(
+//   source1$,
+//   source2$
+// ).take(25)
+// .subscribe(getSubscriber("merge two"));
 
-// Rx.Observable.fromEvent(input, 'keyup')
-//   .map(e => e.target.value )
-//   .distinct()
-//   //.debounceTime(2000)
-//   .subscribe(getSubscriber("debounce"));
+// Rx.Observable.range(0, 3)
+//   .map(function (x) {
+//     return Rx.Observable.range(0, 3);
+//   })
+//   .mergeAll()
+//   .subscribe(getSubscriber("mergeAll"));
 
-_rxjs2.default.Observable.of(23, 23, 55, 43, 67, 55, 44, 43, 23).distinctUntilChanged().subscribe((0, _getSubscriber.getSubscriber)("distinct"));
+var source1$ = _rxjs2.default.Observable.range(1, 5).map(function (i) {
+  return 'source 1 ' + i;
+});
+var source2$ = _rxjs2.default.Observable.range(4, 8).map(function (i) {
+  return 'source 2 ' + i;
+});
+
+// Rx.Observable.concat(source1$, source2$)
+//   .subscribe(getSubscriber("concat"));
+
+// Rx.Observable.concat(source1$, source2$)
+//   .subscribe(getSubscriber("concat"));
+
+_rxjs2.default.Observable.range(0, 3).map(function (x) {
+  return _rxjs2.default.Observable.range(x, 3);
+}).concatAll().subscribe((0, _getSubscriber.getSubscriber)("concatAll"));
 
 /***/ }),
 /* 70 */
--- src/app.js
+++ src/app.js
@@ -2,50 +2,45 @@ import $ from 'jquery';
 import Rx from 'rxjs';
 import {getSubscriber} from './utils/getSubscriber';
 
-// Rx.Observable.range(0, 10)
-//   .filter(function (x, i, obs) {
-//     return x >= 5;
-//   })
-//   .subscribe(getSubscriber("filter"));
+// Rx.Observable.of("hello")
+//   .merge(Rx.Observable.of("world"))
+//   .subscribe(getSubscriber("merge"));
 
-// Rx.Observable.range(0, 10)
-//   .filter(function (x, i, obs) {
-//     return x % 2 === 0;
-//   })
-//   .subscribe(getSubscriber("filter"));
+// Rx.Observable.interval(2000)
+//   .merge(Rx.Observable.interval(500))
+//   .take(25)
+//   .subscribe(getSubscriber("merge"));
 
-let input = $('#sku');
-let output = $('#output');
-
-// let products = [
-//   {sku:"001", name: "red t-shirt", price: 4.99},
-//   {sku:"002", name: "blue pants", price: 9.99},
-//   {sku:"003", name: "green hat", price: 7.99},
-//   {sku:"003", name: "yellow t-shirt", price: 4.99},
-// ];
+//let source1$ = Rx.Observable.interval(2000).map(i => 'M1: ' + i);
+//let source2$ = Rx.Observable.interval(500).map(i => 'M2: ' + i);
 //
-// Rx.Observable.fromEvent(input, 'keyup')
-//   .map(e => e.target.value)
-//   .subscribe(x => {
-//     Rx.Observable.from(products)
-//       .filter(i => i.sku == x)
-//       .subscribe(x => {
-//         output.html('<h1>'+x.name+'</h1><h3>$'+x.price+'</h3>')
-//       })
-//   })
-//
-// Rx.Observable.from(products)
-//   .filter(i => i.price < 5)
-//   .subscribe(x => {
-//     console.log(x)
-//   })
+// Rx.Observable.merge(
+//   source1$,
+//   source2$
+// ).take(25)
+// .subscribe(getSubscriber("merge two"));
 
-// Rx.Observable.fromEvent(input, 'keyup')
-//   .map(e => e.target.value )
-//   .distinct()
-//   //.debounceTime(2000)
-//   .subscribe(getSubscriber("debounce"));
+// Rx.Observable.range(0, 3)
+//   .map(function (x) {
+//     return Rx.Observable.range(0, 3);
+//   })
+//   .mergeAll()
+//   .subscribe(getSubscriber("mergeAll"));
 
-Rx.Observable.of(23,23,55,43,67,55,44,43,23)
-  .distinctUntilChanged()
-  .subscribe(getSubscriber("distinct"));
+let source1$ = Rx.Observable.range(1, 5).map(i => 'source 1 '+i);
+let source2$ = Rx.Observable.range(4, 8).map(i => 'source 2 '+i);
+
+// Rx.Observable.concat(source1$, source2$)
+//   .subscribe(getSubscriber("concat"));
+
+// Rx.Observable.concat(source1$, source2$)
+//   .subscribe(getSubscriber("concat"));
+
+Rx.Observable.range(0, 3)
+  .map(function (x) {
+    return Rx.Observable.range(x, 3);
+    
+  })
+  .concatAll()
+  .subscribe(getSubscriber("concatAll"));
+  
